29527267992ad8bf526d14c82ca7ebb551699ed5
Henry <henry.fabio2005@hotmail.com>
2020-10-07 00:13:36 -0300

    Init Project

---
+++ src/main/java/com/nextplugin/nextmarket/NextMarket.java
@@ -0,0 +1,21 @@
+package com.nextplugin.nextmarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NextMarket extends JavaPlugin {
+
+    public static NextMarket getInstance() {
+        return getPlugin(NextMarket.class);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
