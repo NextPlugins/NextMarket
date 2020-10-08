e1ace5835a2feae55dbcc57e3092e0cdda68cbd4
Henry <henry.fabio2005@hotmail.com>
2020-10-08 17:08:28 -0300

    feat(core): delete old category loader

--- src/main/java/com/nextplugin/nextmarket/api/market/Market.java
+++ src/main/java/com/nextplugin/nextmarket/api/market/Market.java
@@ -1,51 +1,5 @@
 package com.nextplugin.nextmarket.api.market;
 
-import com.nextplugin.nextmarket.NextMarket;
-import com.nextplugin.nextmarket.api.item.MarketItem;
-import com.nextplugin.nextmarket.api.category.Category;
-import com.nextplugin.nextmarket.api.category.loader.CategoryLoader;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public interface Market {
-
-    List<Category> categories = new CategoryLoader(NextMarket.getInstance()).load();
-
-    default List<MarketItem> getMarketItemsFrom(Player player) {
-        List<MarketItem> items = new ArrayList<>();
-
-        for (Category category : getCategories()) {
-            items.addAll(category
-                    .getItems()
-                    .stream()
-                    .filter(marketItem ->
-                            marketItem
-                            .getPlayer()
-                            .getName()
-                            .equals(player.getName()))
-                    .collect(Collectors.toList()));
-        }
-
-        return items;
-    }
-
-    default List<Category> getCategories() {
-        return categories;
-    }
-
-    default Category getCategoryToItem(ItemStack itemStack) {
-        Material material = itemStack.getType();
-
-        for (Category category : getCategories()) {
-            if (category.getValidTypes().contains(material)) return category;
-        }
-
-        return null;
-    }
 
 }
