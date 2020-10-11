efe6f96f24d6ae198fa23728ae00e3516e4886b3
Henry <henry.fabio2005@hotmail.com>
2020-10-11 16:31:35 -0300

    refactor(core): remove unused NumberUtil methods

--- src/main/java/com/nextplugin/nextmarket/util/NumberUtil.java
+++ src/main/java/com/nextplugin/nextmarket/util/NumberUtil.java
@@ -13,43 +13,10 @@ import java.util.Random;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class NumberUtil {
 
-    private final static Random random = new Random();
     private final static DecimalFormat numberFormat = new DecimalFormat("#,###.#");
-
-    public static Integer getInt(String str) {
-        return parseNumber(str, Integer::parseInt);
-    }
-
-    public static Long getLong(String str) {
-        return parseNumber(str, Long::parseLong);
-    }
-
-    public static Float getFloat(String str) {
-        return parseNumber(str, Float::parseFloat);
-    }
 
     public static Double getDouble(String str) {
         return parseNumber(str, Double::parseDouble);
-    }
-
-    public static Byte getByte(String str) {
-        return parseNumber(str, Byte::parseByte);
-    }
-
-    public static Short getShort(String str) {
-        return parseNumber(str, Short::parseShort);
-    }
-
-    public static int getRandomInt(Random random, int max) {
-        return random.nextInt(max);
-    }
-
-    public static int getRandomInt(int max) {
-        return getRandomInt(random, max);
-    }
-
-    public static boolean checkChance(int chance) {
-        return getRandomInt(100) <= chance;
     }
 
     public static String formatNumber(Number number) {
