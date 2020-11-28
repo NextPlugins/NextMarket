7ca1990dfa31e1e758471da40beb57f013964fa9
Henry <henry.fabio2005@hotmail.com>
2020-11-27 23:33:48 -0300

    init refactor

--- src/main/java/com/nextplugin/nextmarket/api/item/QueueItemOperation.java
+++
@@ -1,6 +0,0 @@
-package com.nextplugin.nextmarket.api.item;
-
-public enum QueueItemOperation {
-
-    INSERT, DELETE
-}
--- src/main/java/com/nextplugin/nextmarket/sql/provider/document/parser/DocumentParser.java
+++
@@ -1,9 +0,0 @@
-package com.nextplugin.nextmarket.sql.provider.document.parser;
-
-import com.nextplugin.nextmarket.sql.provider.document.Document;
-
-public interface DocumentParser<T> {
-
-    T parse(Document document);
-
-}
