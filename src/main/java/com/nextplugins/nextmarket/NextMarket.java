2535f99b83e3fe32c1ea500a424c4bb6a56ef6bc
Yuhtin <rio.daviduarte@gmail.com>
2021-05-14 12:24:52 -0300

    fix: using pdm in more repos & timed startup

--- src/main/java/com/nextplugins/nextmarket/NextMarket.java
+++ src/main/java/com/nextplugins/nextmarket/NextMarket.java
@@ -1,5 +1,6 @@
 package com.nextplugins.nextmarket;
 
+import com.google.common.base.Stopwatch;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.name.Named;
@@ -23,6 +24,7 @@ import com.nextplugins.nextmarket.manager.ProductManager;
 import com.nextplugins.nextmarket.registry.InventoryButtonRegistry;
 import com.nextplugins.nextmarket.registry.InventoryRegistry;
 import lombok.Getter;
+import lombok.val;
 import me.bristermitten.pdm.PluginDependencyManager;
 import me.saiintbrisson.bukkit.command.BukkitFrame;
 import org.bstats.bukkit.Metrics;
@@ -34,6 +36,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public final class NextMarket extends JavaPlugin {
@@ -60,40 +63,60 @@ public final class NextMarket extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        PluginDependencyManager.of(this).loadAllDependencies().thenRun(() -> {
-            try {
-                this.categoriesConfig = ConfigurationLoader.of("categories.yml").saveResource().create();
-                InventoryManager.enable(this);
+        getLogger().info("Baixando e carregando dependências necessárias...");
 
-                configureSqlProvider();
-                this.sqlProvider.connect();
+        val downloadTime = Stopwatch.createStarted();
 
-                this.injector = PluginModule.of(this).createInjector();
-                this.injector.injectMembers(this);
+        PluginDependencyManager.of(this)
+                .loadAllDependencies()
+                .exceptionally(throwable -> {
 
-                economyHook.init();
+                    throwable.printStackTrace();
 
-                categoryManager.init();
-                productManager.init();
+                    getLogger().severe("Ocorreu um erro durante a inicialização do plugin!");
+                    Bukkit.getPluginManager().disablePlugin(this);
 
-                inventoryRegistry.init();
-                inventoryButtonRegistry.init();
+                    return null;
 
-                enableCommandFrame();
+                })
+                .join();
 
-                registerListener(ProductCreateListener.class);
-                registerListener(ProductRemoveListener.class);
-                registerListener(ProductBuyListener.class);
+        downloadTime.stop();
 
-                configureBStats();
+        getLogger().log(Level.INFO, "Dependências carregadas com sucesso! ({0})", downloadTime);
+        getLogger().info("Iniciando carregamento do plugin.");
 
-                this.injector.injectMembers(NextMarketAPI.getInstance());
-            } catch (Throwable t) {
-                t.printStackTrace();
-                logger.severe("Um erro ocorreu na inicialização do plugin!");
-                Bukkit.getPluginManager().disablePlugin(this);
-            }
-        });
+        val loadTime = Stopwatch.createStarted();
+
+        this.categoriesConfig = ConfigurationLoader.of("categories.yml").saveResource().create();
+        InventoryManager.enable(this);
+
+        configureSqlProvider();
+        this.sqlProvider.connect();
+
+        this.injector = PluginModule.of(this).createInjector();
+        this.injector.injectMembers(this);
+
+        economyHook.init();
+
+        categoryManager.init();
+        productManager.init();
+
+        inventoryRegistry.init();
+        inventoryButtonRegistry.init();
+
+        enableCommandFrame();
+
+        registerListener(ProductCreateListener.class);
+        registerListener(ProductRemoveListener.class);
+        registerListener(ProductBuyListener.class);
+
+        configureBStats();
+
+        this.injector.injectMembers(NextMarketAPI.getInstance());
+
+        loadTime.stop();
+        getLogger().log(Level.INFO, "Plugin inicializado com sucesso. ({0})", loadTime);
     }
 
     public static NextMarket getInstance() {
