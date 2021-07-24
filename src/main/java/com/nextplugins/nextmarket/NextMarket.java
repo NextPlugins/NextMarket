daa7c5c2788b0ea2b62071874eb32872020cbf2c
Yuhtin <rio.daviduarte@gmail.com>
2021-07-24 20:18:40 -0300

    feat: REMOVE PDM OOoOoooOo

--- src/main/java/com/nextplugins/nextmarket/NextMarket.java
+++ src/main/java/com/nextplugins/nextmarket/NextMarket.java
@@ -3,11 +3,11 @@ package com.nextplugins.nextmarket;
 import com.google.common.base.Stopwatch;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
-import com.google.inject.name.Named;
 import com.henryfabio.minecraft.inventoryapi.manager.InventoryManager;
 import com.henryfabio.sqlprovider.connector.SQLConnector;
 import com.henryfabio.sqlprovider.executor.SQLExecutor;
 import com.nextplugins.nextmarket.api.NextMarketAPI;
+import com.nextplugins.nextmarket.api.metric.MetricProvider;
 import com.nextplugins.nextmarket.command.MarketCommand;
 import com.nextplugins.nextmarket.configuration.ConfigurationLoader;
 import com.nextplugins.nextmarket.dao.SQLProvider;
@@ -22,24 +22,19 @@ import com.nextplugins.nextmarket.registry.InventoryButtonRegistry;
 import com.nextplugins.nextmarket.registry.InventoryRegistry;
 import lombok.Getter;
 import lombok.val;
-import me.bristermitten.pdm.PluginDependencyManager;
 import me.saiintbrisson.bukkit.command.BukkitFrame;
-import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public final class NextMarket extends JavaPlugin {
 
     @Getter private Injector injector;
     @Getter private SQLConnector sqlConnector;
     @Getter private SQLExecutor sqlExecutor;
-
-    @Inject @Named("main") private Logger logger;
 
     @Inject private EconomyHook economyHook;
 
@@ -58,27 +53,6 @@ public final class NextMarket extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Baixando e carregando dependências necessárias...");
-
-        val downloadTime = Stopwatch.createStarted();
-
-        PluginDependencyManager.of(this)
-                .loadAllDependencies()
-                .exceptionally(throwable -> {
-
-                    throwable.printStackTrace();
-
-                    getLogger().severe("Ocorreu um erro durante a inicialização do plugin!");
-                    Bukkit.getPluginManager().disablePlugin(this);
-
-                    return null;
-
-                })
-                .join();
-
-        downloadTime.stop();
-
-        getLogger().log(Level.INFO, "Dependências carregadas com sucesso! ({0})", downloadTime);
         getLogger().info("Iniciando carregamento do plugin.");
 
         val loadTime = Stopwatch.createStarted();
@@ -106,7 +80,7 @@ public final class NextMarket extends JavaPlugin {
         registerListener(ProductRemoveListener.class);
         registerListener(ProductBuyListener.class);
 
-        configureBStats();
+        MetricProvider.of(this).register();
 
         this.injector.injectMembers(NextMarketAPI.getInstance());
 
@@ -127,12 +101,6 @@ public final class NextMarket extends JavaPlugin {
 
     private void registerListener(Class<? extends Listener> clazz) {
         Bukkit.getPluginManager().registerEvents(injector.getInstance(clazz), this);
-    }
-
-    private void configureBStats() {
-        int pluginId = 9933;
-        new Metrics(this, pluginId);
-        logger.info("Integração com o bStats configurada com sucesso.");
     }
 
 }
---
+++ src/main/java/com/nextplugins/nextmarket/api/metric/MetricProvider.java
@@ -0,0 +1,20 @@
+package com.nextplugins.nextmarket.api.metric;
+
+import lombok.Data;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Data(staticConstructor = "of")
+public final class MetricProvider {
+
+    private final JavaPlugin plugin;
+
+    public void register() {
+
+        System.setProperty("bstats.relocatecheck", "false");
+
+        new MetricsConnector(plugin, 10041);
+        plugin.getLogger().info("Métrica de uso habilitada com sucesso.");
+
+    }
+
+}
